1a5bc8efa250c5c9417af8b9725907d323302d60
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-01-16 07:45:36 +0800

    Add feat: simple query form
    Missed one line to staged

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,20 @@
 const API_KEY = "f23670da91c3a4b07ec78478022186d0";
 
+const queryForm = () => {
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  const submit = document.createElement("button");
+  submit.textContent = "Search City";
+
+  const form = document.createElement("form");
+  form.appendChild(input);
+  form.appendChild(submit);
+  return form;
+};
+
+const body = document.querySelector("body");
+body.appendChild(queryForm());
+
 const callApi = async (queryCity) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${queryCity}&APPID=${API_KEY}`
