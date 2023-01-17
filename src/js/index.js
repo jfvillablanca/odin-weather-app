ef69d25247daf5ecd179faa5e8959d12397a167f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-01-17 12:47:54 +0800

    Delete unnecessary DOM element creation

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,5 @@
+
 const API_KEY = "f23670da91c3a4b07ec78478022186d0";
-
-const queryForm = () => {
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
-  const submit = document.createElement("button");
-  submit.textContent = "Search City";
-
-  const form = document.createElement("form");
-  form.appendChild(input);
-  form.appendChild(submit);
-  return form;
-};
-
-const body = document.querySelector("body");
-body.appendChild(queryForm());
 
 const queryLocation = async (location) => {
   const response = await fetch(
