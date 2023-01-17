51d549ba40d8bfb7b74a1d8b98b9993a95214181
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-01-17 15:49:32 +0800

    Add new icon imports

--- src/js/images.js
+++ src/js/images.js
@@ -2,5 +2,18 @@ import WeatherIcon from "../images/weather/04d_804.svg";
 import LocationIcon from "../images/map-pin.svg";
 import SunriseIcon from "../images/weather/sys_sunrise.svg";
 import SunsetIcon from "../images/weather/sys_sunset.svg";
+import HumidityIcon from "../images/weather/main_humidity.svg";
+import PressureIcon from "../images/weather/main_pressure.svg";
+import WindSpeedIcon from "../images/weather/wind_speed.svg";
+import WindAngleIcon from "../images/weather/wind_angle.svg";
 
-export { LocationIcon, SunriseIcon, SunsetIcon, WeatherIcon };
+export {
+  HumidityIcon,
+  LocationIcon,
+  PressureIcon,
+  SunriseIcon,
+  SunsetIcon,
+  WeatherIcon,
+  WindAngleIcon,
+  WindSpeedIcon,
+};
