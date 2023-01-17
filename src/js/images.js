8f667f0999bee570305eed7d85b2616ce3589992
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-01-17 11:28:06 +0800

    Add images.js: aggregator of image imports

---
+++ src/js/images.js
@@ -0,0 +1,6 @@
+import WeatherIcon from "../images/weather/04d_804.svg";
+import LocationIcon from "../images/map-pin.svg";
+import SunriseIcon from "../images/weather/sys_sunrise.svg";
+import SunsetIcon from "../images/weather/sys_sunset.svg";
+
+export { LocationIcon, SunriseIcon, SunsetIcon, WeatherIcon };
